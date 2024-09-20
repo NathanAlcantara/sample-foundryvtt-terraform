3346421168889946aa0d19e100da96daa5f60c4a
NathanAlcantara <nathangabriel97@gmail.com>
2024-09-20 02:54:37 +0000

    update aws provider

--- terraform.tf
+++ terraform.tf
@@ -12,7 +12,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "5.46.0"
+      version = "5.68.0"
     }
     archive = {
       source = "hashicorp/archive"
